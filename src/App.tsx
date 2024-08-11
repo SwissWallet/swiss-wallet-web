10c4f0fe81b57acede5bee0309d603188bae85bf
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-11 18:48:56 -0300

    feat: added new route to RegisterPage

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import {
 } from 'react-router-dom';
 
 import { LoginPage } from './pages/login-page';
+import { RegisterPage } from './pages/register-page';
 
 const router = createBrowserRouter([
-  {path: '/', element: <LoginPage />}
+  {path: '/', element: <LoginPage />},
+  {path: '/register', element: <RegisterPage />},
 ])
 
 function App() {
---
+++ src/pages/register-page/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderLoginAndRegister } from "../../components/macro-components/header-login-and-register";
+
+export function RegisterPage(){
+    return(
+        <div className="bg-red-gradient h-screen w-full">
+            <HeaderLoginAndRegister />
+            <main className="flex justify-center">
+
+                <form className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
+
+                </form>
+                
+            </main>
+        </div>
+    )
+}
