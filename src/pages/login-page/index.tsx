87265b6c063450a24244038213f1d2b01419178d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-11 15:53:44 -0300

    created HeaderLoginAndRegister component

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,22 +1,17 @@
-import logoSwiss from '../../assets/images/logo-swisswallet.png'
-import logoSenai from '../../assets/images/logo-senai.png'
-import { CircleHelp } from 'lucide-react'
+import { HeaderLoginAndRegister } from "../../components/header-login-and-register";
+
 
 export function LoginPage(){
     return(
         <div className="h-screen w-full bg-red-gradient">
-            
-            <div className='flex p-5 justify-between'>
-                <div className='flex items-center space-x-5'>
-                    <img className='w-60' src={logoSwiss} alt='logo da marca' />                    
-                    <div className='w-[1px] h-16 bg-white' />
-                    <img className='w-[180px] h-[45px]' src={logoSenai} alt="logo senai" />
-                </div>
-                <div>
-                    <CircleHelp className='text-white size-10'/>
-                </div>
-            </div>
+            <HeaderLoginAndRegister />
 
+            <main className="flex justify-center">
+                <form className="bg-white rounded-lg w-[500px] h-auto p-5">
+                    <h1>Login</h1>
+                </form>
+            </main>
+            
         </div>
 
 
