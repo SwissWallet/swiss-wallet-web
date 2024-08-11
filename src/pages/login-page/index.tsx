d1fd7c7d9e3c5ae5c2066a9aee43d95b203cc773
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-11 17:05:44 -0300

    feat: created UserPasswordModal component

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,18 +1,38 @@
 import { useState } from "react";
 import { HeaderLoginAndRegister } from "../../components/header-login-and-register";
 import { UsernameModal } from "./username-modal";
+import { UserPasswordModal } from "./userpassword-modal";
 
 
 export function LoginPage(){
 
-    
+    const [ filledUserName, setFilledUserName ] = useState(false)
+
+    function handdleAdvanceUserInput(){
+        setFilledUserName(true);
+    }
+
+    function handdleBackUserInput(){
+        setFilledUserName(false);
+    }
 
     return(
         <div className="h-screen w-full bg-red-gradient">
             <HeaderLoginAndRegister />
 
             <main className="flex justify-center">
-                <UsernameModal />
+
+                {
+                    filledUserName ? (
+                        <UserPasswordModal 
+                            handdleBackUserInput={handdleBackUserInput}
+                        />
+                    ) : (
+                        <UsernameModal 
+                            handdleAdvanceUserInput={handdleAdvanceUserInput}
+                        />
+                    )
+                }
             </main>
             
         </div>
--- src/pages/login-page/userpassword-modal.tsx
+++ src/pages/login-page/userpassword-modal.tsx
@@ -1,34 +1,42 @@
-interface UsernameModalProps{
-    handdleAdvanceUserInput: () => void,
+import { ChevronLeft } from "lucide-react";
+
+interface UserPasswordModalProps{
+    handdleBackUserInput: () => void,
 }
 
-export function UsernameModal({
-    handdleAdvanceUserInput,
-}:UsernameModalProps){
+export function UserPasswordModal({
+    handdleBackUserInput
+}:UserPasswordModalProps){
     return(
                 <form className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
+
+                    <div className=" flex items-center justify-start">
+                        <button onClick={handdleBackUserInput} className="rounded-full bg-default-red hover:bg-red-700 w-8 h-8 flex items-center justify-center">
+                            <ChevronLeft className="size-6 text-white " />
+                        </button>
+                    </div>
+
                     <div className="flex flex-col gap-3">
-                        <h1 className="text-4xl font-medium">Login</h1>
-                        <p className="font-medium text-sm text-zinc-800 ml-4">Bem-vindo(a) ao portal SwissWallet.</p>
+                        <h3 className="text-3xl font-medium">Digite sua senha</h3>
+                        <p className="font-medium text-sm text-zinc-800 ml-4">usernameusuario@senaisp</p>
                     </div>
                     <div className="flex flex-col gap-3">
-                        <h3 className="text-lg font-medium">Usuário</h3>
                         <div className="flex justify-center flex-col items-center gap-1">
-                            <input type="email" placeholder="Insira seu e-mail" 
+                            <input type="email" placeholder="Insira sua senha" 
                                 className="outline-none rounded-md p-2 w-full border-2 border-zinc-300  font-medium placeholder-slate-400
                                     focus:not-italic focus:border-red-600 placeholder:font-light placeholder:italic" 
                             />
                             <a>
                                 <span className="text-sm font-medium text-zinc-500
                                         hover:text-zinc-600 hover:cursor-pointer">
-                                    não tenho conta
+                                    esqueceu sua senha?
                                 </span>
                             </a>
                         </div>
                     </div>
                     <div className="flex justify-center items-center">
-                        <button onClick={handdleAdvanceUserInput} className="bg-default-red py-2 px-20 rounded-md hover:bg-red-700">
-                            <span className="font-medium text-white text-lg">Avançar</span>
+                        <button className="bg-default-red py-2 px-20 rounded-md hover:bg-red-700">
+                            <span className="font-medium text-white text-lg">Acessar</span>
                         </button>
                     </div>
                 </form>
