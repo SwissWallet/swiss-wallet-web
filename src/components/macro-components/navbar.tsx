ca3a4b5db1f385f11567a72abee3f0fd85c7281f
PedroVieiraHO <pedrovieirah36@gmail.com>
2024-08-13 22:03:43 -0300

    feat: created card header

--- src/components/macro-components/navbar.tsx
+++ src/components/macro-components/navbar.tsx
@@ -3,13 +3,27 @@ import logo from "../../assets/images/logo-swisswallet.png"
 
 export function Navbar(){
     return(
-        <div>
-            <nav className="flex bg-red-gradient justify-between p-5 items-center text-white text-2xl px-24 font-semibold">
+        <>
+        <header className="bg-red-gradient flex flex-col">
+            <nav className="flex justify-between items-center p-10 text-white text-2xl px-24 font-semibold">
                 <img src={logo} alt="teste" />
                 <a>Swiss Store</a>
                 <a>Biblioteca</a>
                 <a>Cantina</a>
             </nav>
-        </div>
+        </header>
+            <section className="flex justify-center -mt-5 ">
+                <div className="bg-white text-black p-10 w-[600px] font-bold rounded-xl shadow-lg">
+                    <h4 className="">Saldo</h4>
+                    <div className="flex justify-between pt-2">
+                        <h1 className="text-xl font-extrabold">Pontos<span>     8409382</span></h1>
+                        <img src="" alt="" />
+                        <div>
+                            <p>ver extrato</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
