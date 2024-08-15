551f024b98661b992855a94dfa785051dbe0fedf
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-15 20:52:47 -0300

    style: added variants styles on MainButton component

--- src/components/macro-components/destaques.tsx
+++ src/components/macro-components/destaques.tsx
@@ -1,4 +1,5 @@
 import CamisaBranca from "../../assets/images/camisa-branca.svg"
+import { MainButton } from "../micro-components/main-button"
 
 
 export function CardDestaques(){
@@ -6,15 +7,19 @@ export function CardDestaques(){
         <div className="bg-white p-10 w-[1200px] rounded-xl shadow-lg mt-24">
             <h3 className="text-4xl font-extrabold flex justify-between mb-8">Destaques - Loja</h3>
             <div className="inline-block">
-                <img src={CamisaBranca} alt="camiseta branca com logo do senai" />
-                <article className="bg-black text-white p-4 rounded-lg">
-                    <h4 className="text-xl font-semibold">Camiseta Destaque</h4>
-                    <p className="text-sm font-extralight">Descrição da camiseta destaque</p>
-                    <div className="flex justify-between">
-                        <h4 className="font-extrabold text-4xl mt-">40<span className="text-sm ml-1">pontos</span></h4>
-                        <button className="bg-[#C40601] px-6 py-1 rounded-3xl">ver mais</button>
-                    </div>
-                </article>
+                <div className="flex items-center flex-col">
+                    <img src={CamisaBranca} alt="camiseta branca com logo do senai" />
+                    <article className="bg-black text-white p-4 rounded-lg gap-4 flex flex-col -mt-36">
+                        <div className="flex flex-col gap-1">
+                            <h4 className="text-xl font-semibold">Camiseta Destaque</h4>
+                            <p className="text-sm font-extralight">Descrição da camiseta destaque</p>
+                        </div>
+                        <div className="flex justify-between gap-10">
+                            <h4 className="font-extrabold text-4xl mt-">40<span className="text-sm ml-1">pontos</span></h4>
+                            <MainButton width="min" >ver mais</MainButton>
+                        </div>
+                    </article>
+                </div>
             </div>
         </div>
     )
