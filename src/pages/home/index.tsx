88c5679303ef8be7f8b57a48ddb26f6c964ca48c
Brennon <brennonmeireles7@gmail.com>
2024-08-14 22:14:29 -0300

    feat: added component card destaques to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import { CardFavoritos } from "../../components/macro-components/card-favoritos"
 import {Navbar} from "../../components/macro-components/navbar"
+import {CardDestaques} from "../../components/macro-components/destaques"
 
 export function Home(){
     return(
         <div>
             <Navbar />
             <CardFavoritos />
+            <CardDestaques />
         </div>
     )
 }
