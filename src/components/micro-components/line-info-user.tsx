73ee7a59daabb6d1dfbf8ef1f8e173b1d5615155
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-15 22:12:17 -0300

    feat: created info user component

---
+++ src/components/micro-components/line-info-user.tsx
@@ -0,0 +1,13 @@
+export function InfoUser(){
+    return(
+                        <div className="flex flex-col p-5 gap-4">
+                            <div className="flex justify-between">
+                                <h3 className="flex flex-1 text-xl font-normal">Nome:</h3>
+                                <div className="flex w-full justify-center">
+                                    <h1 className="text-2xl font-medium">usuário nascimento</h1>
+                                </div>
+                            </div>
+                            <div className="w-full h-[1px] bg-slate-400 " />
+                        </div>
+    )
+}
--- src/pages/user-account/index.tsx
+++ src/pages/user-account/index.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "../../components/macro-components/footer";
 import { Navbar } from "../../components/macro-components/navbar";
+import { InfoUser } from "../../components/micro-components/line-info-user";
 import { UpdateButton } from "../../components/micro-components/update-button";
 
 export function UserAccount(){
@@ -19,6 +20,7 @@ export function UserAccount(){
                         <div className="flex justify-end" >
                             <UpdateButton/>
                         </div>
+                        <InfoUser />
                     </div>
                 </section>
 
