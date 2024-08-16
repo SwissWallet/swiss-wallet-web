71397c5480c3b2058695f0ee1fdcb207fccb7243
Brennon <brennonmeireles7@gmail.com>
2024-08-15 21:16:45 -0300

    feat: created footer component

---
+++ src/components/macro-components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer(){
+    return(
+        <footer  className="bg-red-gradient flex p-7 text-white justify-between italic">
+            <p>© 2024 Senai. Todos os direitos reservados.</p>
+            <div className="flex gap-5">
+                <p>Politica de Privacidade </p>
+                <p>Termos de Uso</p>
+                <p>Contatos</p>
+            </div>
+        </footer>
+    )
+}
