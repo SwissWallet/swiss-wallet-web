dabe3472c5f549adfe260f7007cc61e147cf32c4
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-16 16:16:40 -0300

    feat: finally account page

--- src/components/micro-components/line-info-user.tsx
+++ src/components/micro-components/line-info-user.tsx
@@ -1,10 +1,18 @@
-export function InfoUser(){
+interface InfoUserProps{
+    label: string,
+    value: string,
+}
+
+export function InfoUser({
+    label,
+    value,
+}:InfoUserProps){
     return(
                         <div className="flex flex-col p-5 gap-4">
                             <div className="flex justify-between">
-                                <h3 className="flex flex-1 text-xl font-normal">Nome:</h3>
+                                <h3 className="flex flex-1 text-xl font-normal">{label}:</h3>
                                 <div className="flex w-full justify-center">
-                                    <h1 className="text-2xl font-medium">usuário nascimento</h1>
+                                    <h1 className="text-2xl font-medium">{value}</h1>
                                 </div>
                             </div>
                             <div className="w-full h-[1px] bg-slate-400 " />
--- src/pages/user-account/index.tsx
+++ src/pages/user-account/index.tsx
@@ -8,7 +8,7 @@ export function UserAccount(){
         <div className="bg-white-90">
             <Navbar />
 
-            <main className="ml-20 mr-20">
+            <main className="ml-20 mr-20 gap-20 flex flex-col mt-20 mb-20">
 
                 <div className="flex flex-col justify-start">
                     <h1 className="font-bold text-4xl">Dados pessoais</h1>
@@ -16,11 +16,35 @@ export function UserAccount(){
                 </div>
 
                 <section className="ml-20 mr-20">
+                    <div className="flex flex-col bg-white p-5 drop-shadow-2xl shadow-black rounded-md">
+                        <div className="flex justify-end" >
+                            <UpdateButton/>
+                        </div>
+                        <InfoUser label="Nome" value="Usuário" />
+                        <InfoUser label="Data de nascimento" value="25/07/2000" />
+                        <InfoUser label="CPF" value="12345678910" />
+                        <InfoUser label="Telefone" value="11946415527" />
+                    </div>
+                </section>
+                <section className="ml-20 mr-20">
                     <div className="flex flex-col bg-white p-5 shadow-black shadow-md rounded-md">
                         <div className="flex justify-end" >
                             <UpdateButton/>
                         </div>
-                        <InfoUser />
+                        <InfoUser label="Cidade" value="São Paulo" />
+                        <InfoUser label="Bairro" value="Centro" />
+                        <InfoUser label="CEP" value="11590-130" />
+                        <InfoUser label="Rua" value="Conselheiro Crispianiano" />
+                        <InfoUser label="Complemento" value="Apto 202, Bloco B" />
+                    </div>
+                </section>
+                <section className="ml-20 mr-20">
+                    <div className="flex flex-col bg-white p-5 shadow-black shadow-md rounded-md">
+                        <div className="flex justify-end" >
+                            <UpdateButton/>
+                        </div>
+                        <InfoUser label="E-mail" value="username@senaisp" />
+                        <InfoUser label="Senha" value="******12" />
                     </div>
                 </section>
 
