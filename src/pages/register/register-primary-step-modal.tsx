a673759398938266b91da84b0e5f70b95fc25f18
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-17 15:59:11 -0300

    style: update style

--- src/pages/register/register-primary-step-modal.tsx
+++ src/pages/register/register-primary-step-modal.tsx
@@ -12,36 +12,45 @@ export function RegisterPrimaryStep({
     finishedPrimaryStep,
     handdleChange,
 }:RegisterPrimaryStepPros){
+
+    const handdleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        finishedPrimaryStep();
+    }
+
     return(
-                <form className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
 
-                    <Link to={'/'}>
-                        <BackButton />
-                    </Link>
-                    <div className="flex flex-col gap-3">
-                        <h1 className="text-4xl font-medium">Cadastre-se</h1>
-                        <p className="font-medium text-sm text-zinc-600 ml-4">Todos os campos são obrigatórios</p>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <div className="bg-default-red h-10 w-10 rounded-full"></div>
-                        <div className="bg-dark-gray h-[2px] w-16"></div>
-                        <div className="bg-dark-gray h-10 w-10 rounded-full"></div>
-                        <div className="bg-dark-gray h-[2px] w-16"></div>
-                        <div className="bg-dark-gray h-10 w-10 rounded-full"></div>
-                    </div>
-                    <div className="flex flex-col gap-6">
-                        <UserInput placeholder="ex: José da Silva" type="text" name="name" onChange={handdleChange}>Nome completo</UserInput>
-                        <UserInput type="date" name="dateBorn" onChange={handdleChange}>Data de nascimento</UserInput>
-                        <UserInput placeholder="ex: jose.silva@senaisp" type="email" name="email" onChange={handdleChange}>E-mail</UserInput>
-                        <UserInput placeholder="ex: 12345678910" type="number" name="cpf" onChange={handdleChange} >CPF</UserInput>
-                        <UserInput placeholder="ex: 11991827364" type="number" name="phone" onChange={handdleChange}>Telefone</UserInput>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <MainButton onClick={finishedPrimaryStep} >
-                            Avançar
-                        </MainButton>
-                    </div>
+        <div className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
+            <Link to={'/'}>
+                <BackButton />
+            </Link>
+            <form onSubmit={handdleSubmit} className="flex gap-8 flex-col">
 
-                </form>
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-4xl font-medium">Cadastre-se</h1>
+                    <p className="font-medium text-sm text-zinc-600 ml-4">Todos os campos são obrigatórios</p>
+                </div>
+                <div className="flex items-center justify-center">
+                    <div className="bg-default-red h-10 w-10 rounded-full"></div>
+                    <div className="bg-dark-gray h-[2px] w-16"></div>
+                    <div className="bg-dark-gray h-10 w-10 rounded-full"></div>
+                    <div className="bg-dark-gray h-[2px] w-16"></div>
+                    <div className="bg-dark-gray h-10 w-10 rounded-full"></div>
+                </div>
+                <div className="flex flex-col gap-6">
+                    <UserInput placeholder="ex: José da Silva" type="text" name="name" onChange={handdleChange}>Nome completo</UserInput>
+                    <UserInput type="date" name="dateBorn" onChange={handdleChange}>Data de nascimento</UserInput>
+                    <UserInput placeholder="ex: jose.silva@senaisp" type="email" name="email" onChange={handdleChange}>E-mail</UserInput>
+                    <UserInput placeholder="ex: 12345678910" type="number" name="cpf" onChange={handdleChange} >CPF</UserInput>
+                    <UserInput placeholder="ex: 11991827364" type="number" name="phone" onChange={handdleChange}>Telefone</UserInput>
+                </div>
+                <div className="flex justify-center items-center">
+                    <MainButton type="submit" >
+                        Avançar
+                    </MainButton>
+                </div>
+
+            </form>
+        </div>
     )
 }
