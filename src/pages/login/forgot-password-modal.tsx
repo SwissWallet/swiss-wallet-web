5cd1d1efb6e47fa4dc3aafe2ef6ccff18b1de6ea
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-08-18 22:16:23 -0300

    feat: added option visible password on forgor-password-modal component

--- src/pages/login/forgot-password-modal.tsx
+++ src/pages/login/forgot-password-modal.tsx
@@ -1,5 +1,7 @@
+import { Eye, EyeOff } from "lucide-react";
 import { MainButton } from "../../components/micro-components/main-button";
 import { UserInput } from "../../components/micro-components/user-input";
+import { useState } from "react";
 
 interface ForgotPasswordProps{
     closeForgotPassword: () => void,
@@ -9,6 +11,12 @@ export function ForgotPassword({
     closeForgotPassword,
 }:ForgotPasswordProps){
 
+    const [ isVisibleNewPassword, setIsVisibleNewPassword ] = useState(false)
+
+    function handdleVisibleNewPassword(){
+        setIsVisibleNewPassword(!isVisibleNewPassword)
+    }
+
     const handdleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         closeForgotPassword();
@@ -16,12 +24,28 @@ export function ForgotPassword({
 
     return(
         <form onSubmit={handdleSubmit} className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
-            <p className="font-medium text-center text-base italic text-zinc-500">Um e-mail será enviado para <span>usernameusuario@senaisp</span>, 
-            prosseguindo com os seguintes procedimentos para a recuperação de senha.</p>
+            <p className="font-medium text-center text-base italic text-zinc-500">
+                Um e-mail será enviado para <span>usernameusuario@senaisp</span>, 
+                prosseguindo com os seguintes procedimentos para a recuperação de senha.</p>
 
             <div className="flex justify-center">
                 <div className="flex flex-col justify-center gap-6 w-96 ">
-                    <UserInput position="center" >
+                    <UserInput 
+                        position="center" 
+                        type={ isVisibleNewPassword ? 'text' : 'password'}
+                        isVisibleSvgIcon={true}
+                        svgIcon={
+                            isVisibleNewPassword ? (
+                                <button onClick={handdleVisibleNewPassword} type="button" className="flex items-center">
+                                    <Eye />
+                                </button>
+                            ) : (
+                                <button onClick={handdleVisibleNewPassword} type="button" className="flex items-center">
+                                    <EyeOff />
+                                </button>
+                            )
+                        }
+                    >
                         Nova senha
                     </UserInput>
                     <UserInput position="center" >
