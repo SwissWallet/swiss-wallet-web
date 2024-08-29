e7e0b72b958eac2c57b8dbacf1801a1d8976e992
Brennon <brennonmeireles7@gmail.com>
2024-08-29 19:11:47 -0300

    feat: created page cardpoints

---
+++ src/pages/card-points/index.tsx
@@ -0,0 +1,13 @@
+import CardPoint from "../../components/macro-components/card-points"
+import { Navbar } from "../../components/macro-components/navbar"
+import { Footer } from "../../components/macro-components/footer"
+
+export default function CardPointPage() {
+    return(
+        <>
+            <Navbar />
+            <CardPoint />
+            <Footer />
+        </>
+    )
+}
