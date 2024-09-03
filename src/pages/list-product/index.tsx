5a927a7a2d0dce9fd0a31390b7d59c4d4a46333c
Brennon <brennonmeireles7@gmail.com>
2024-09-02 22:07:39 -0300

    feat: created new routes

--- src/pages/list-product/index.tsx
+++ src/pages/list-product/index.tsx
@@ -1,12 +1,12 @@
 import {Navbar} from '../../components/macro-components/navbar'
-import {AddProduct} from '../../components/macro-components/list-product'
+import ListProduct from '../../components/macro-components/list-product'
 import {Footer} from '../../components/macro-components/footer'
 
 export default function AddProduct(){
     return(
         <>
             <Navbar />
-            <AddProduct />
+            <ListProduct />
             <Footer />
         </>
     )
