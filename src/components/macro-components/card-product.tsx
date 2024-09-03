e255e43583edc6686c6696fe845246300d3e045d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-03 20:28:10 -0300

    style: fix style on card product component

--- src/components/macro-components/card-product.tsx
+++ src/components/macro-components/card-product.tsx
@@ -20,20 +20,24 @@ export function CardProduct({
     return(
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
             <div className="bg-red-gradient rounded-lg w-[600px] h-auto p-5 flex gap-8 flex-col">
-                <button className="flex justify-end" onClick={closeCardProduct}><X className="size-6 text-white"/></button>
                 <div className="flex">
-                    <div className="bg-white rounded-xl shadow-xl w-auto p-5">
-                        <img src={image} alt="produto" />
+                    <div className="bg-white rounded-xl shadow-xl p-5">
+                        <img className="w-[309px] h-[407px]" src={image} alt="produto" />
                     </div>
                     <article className="flex flex-col justify-between p-5">
-                        <div>
+                        <div className="flex flex-col gap-1">
+                            <div className="flex justify-end">
+                                <button className="flex justify-end" onClick={closeCardProduct}><X className="size-6 text-white"/></button>
+                            </div>
                             <h1 className="text-white font-bold text-2xl">{title}</h1>
                             <h3 className="text-zinc-300">{description}</h3>
                         </div>
                         <div className="text-white">
                             <h1 className="text-6xl font-extrabold">{value}<span className="text-2xl font-semibold">pontos</span></h1>
                         </div>
-                        <MainButton width="min">Favoritar</MainButton>
+                        <div className="flex justify-center">
+                            <MainButton width="min">Favoritar</MainButton>
+                        </div>
                     </article>
                 </div>
             </div>
