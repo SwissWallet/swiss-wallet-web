58fdadc502d7b9dc848ffff3253b97a82d41037d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-03 20:03:26 -0300

    style: update style on modal

--- src/components/macro-components/card-product.tsx
+++ src/components/macro-components/card-product.tsx
@@ -1,3 +1,5 @@
+import { X } from "lucide-react";
+import camisa from "../../assets/images/camisa-branca.svg";
 
 interface CardProductProps{
     closeCardProduct: () => void,
@@ -8,8 +10,18 @@ export function CardProduct({
     closeCardProduct,
 }:CardProductProps){
     return(
-        <div className="block h-auto w-auto">
-            <button onClick={closeCardProduct}>X</button>
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-red-gradient rounded-lg w-[600px] h-auto p-5 flex gap-8 flex-col">
+                <button className="flex justify-end" onClick={closeCardProduct}><X className="size-6 text-white"/></button>
+                <div>
+                    <div className="bg-white rounded-xl shadow-xl w-auto p-5">
+                        <img src={camisa} alt="produto" />
+                    </div>
+                    <article>
+                        
+                    </article>
+                </div>
+            </div>
         </div>
     )
 }
