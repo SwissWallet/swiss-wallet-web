129c809eb17e1f344f4fee4a6bc1f1bf47b87ce2
Brennon <brennonmeireles7@gmail.com>
2024-09-02 22:01:56 -0300

    fix: ajusted names

--- src/pages/card-points/index.tsx
+++ src/pages/card-points/index.tsx
@@ -10,7 +10,6 @@ export default function CardPointPage() {
             <Navbar />
             <CardPoint />
             <Footer />
-            <AddNewProduct />
         </>
     )
 }
---
+++ src/pages/list-product/index.tsx
@@ -0,0 +1,13 @@
+import {Navbar} from '../../components/macro-components/navbar'
+import {AddProduct} from '../../components/macro-components/list-product'
+import {Footer} from '../../components/macro-components/footer'
+
+export default function AddProduct(){
+    return(
+        <>
+            <Navbar />
+            <AddProduct />
+            <Footer />
+        </>
+    )
+}
