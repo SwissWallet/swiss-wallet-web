232a79ec7c02b4b239a8f45302f1cd83526017d6
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-04 17:05:13 -0300

    style: finally basic style on modal

--- src/components/macro-components/deposit-modal.tsx
+++ src/components/macro-components/deposit-modal.tsx
@@ -1,4 +1,6 @@
-import { X } from "lucide-react";
+import { BackButton } from "../micro-components/back-button";
+import { MainButton } from "../micro-components/main-button";
+import { UserInput } from "../micro-components/user-input";
 
 interface DepositModalProps{
     closeDepositModal: () => void,
@@ -10,9 +12,16 @@ export function DepositModal({
     return(
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
             <form className="bg-white rounded-lg w-[600px] h-auto p-5 flex gap-8 flex-col">
-                <button className="size-6 text-zinc-700" onClick={closeDepositModal}>
-                    <X />
-                </button>
+                <BackButton onClick={closeDepositModal}>
+                </BackButton>
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-4xl font-medium">Deposite</h1>
+                    <p className="font-medium text-sm text-zinc-600 ml-4">Todos os campos são obrigatórios</p>
+                </div>
+                <UserInput position="center">E-mail do usuário</UserInput>
+                <UserInput position="center">Depósito</UserInput>
+
+                <MainButton>Depositar</MainButton>
             </form>
         </div>
     )
