a56d9b2681688c22b6480c4e45aab49607718bfa
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-04 16:00:14 -0300

    feat: added consuming api extract

--- src/pages/user-extract/index.tsx
+++ src/pages/user-extract/index.tsx
@@ -5,14 +5,35 @@ import { InfoDateExtract } from "./info-date-extract"
 import { Footer } from "../../components/macro-components/footer"
 import { Navbar } from "../../components/macro-components/navbar"
 import { api } from "../../lib/axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function UserExtract(){
 
-    const [products, setProducts] = useState([]);
-    const response = api.get("/v3/extracts/current").then((json) => {
-        setProducts(json.data);console.log(products)
-    }).catch(error => console.log(error))
+    const extract = {
+        id: "",
+        description: "",
+        value: "",
+        type: "",
+        date: "",
+    }
+
+    const [extractList, setExtractList] = useState([extract]);
+   
+    useEffect(() => {
+        async function getExtracts(){
+            await api.get(`/v3/extracts/current`)
+            .then((json) => {
+                const data = json.data;
+                setExtractList(data.map((item) => ({
+                    id: item.id,
+                    value: item.value,
+                    date: item.date
+                })))
+            })
+        }
+
+        getExtracts()
+    }, [])
 
     return (
         <div className="bg-default-gray"> 
@@ -22,31 +43,20 @@ export function UserExtract(){
             <main className="ml-20 mr-20 gap-10 flex flex-col mt-20 mb-20">
 
                 <HeaderOnPages  title="Extrato" description="Informações do extrato"/>
-
-                <section className="ml-20 mr-20">
-
-                    <div className="flex flex-col bg-white p-5 drop-shadow-custom rounded-md">
-
-                        <div className="pb-4 pl-2"> <InfoDateExtract date="17/Ago/2024"/></div>
-
-                        <InfoExtract  icon={<ChevronUp className="size-8 text-green-500"/>}  value="40" time="12:30"/>
-                        <InfoExtract  icon={<ChevronDown className="size-8 text-red-500"/>}  value="12" time="11:30"/>
-                        <InfoExtract  icon={<ChevronUp className="size-8 text-green-500"/>}  value="10" time="10:30"/>
-                    </div>
-
-                </section>
-
-
+                
                 <section className="ml-20 mr-20">
 
                     <div className="flex flex-col bg-white p-5 drop-shadow-custom rounded-md">
 
                         <div className="pb-4 pl-2"> <InfoDateExtract date="18/Ago/2024"/></div>
-
-                        <InfoExtract  icon={<ChevronUp className="size-8 text-green-500"/>}  value="40" time="12:30"/>
-                        <InfoExtract  icon={<ChevronDown className="size-8 text-red-500"/>}  value="45" time="11:30"/>
-                        <InfoExtract  icon={<ChevronDown className="size-8 text-red-500"/>}  value="24" time="10:30"/>
-                        <InfoExtract  icon={<ChevronUp className="size-8 text-green-500"/>}  value="12" time="09:30"/>
+                        {extractList.map((extract) => (
+                            <div key={extract.id}>
+                                <InfoExtract  
+                                icon={<ChevronUp className="size-8 text-green-500"/>}  
+                                value={extract.value} 
+                                time={extract.date}/>
+                            </div>
+                        ))}
 
                     </div>
 
