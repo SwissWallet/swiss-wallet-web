95eb5a9c0b9e6008c569be25afecb61de5fd63bd
Brennon <brennonmeireles7@gmail.com>
2024-09-05 20:06:59 -0300

    style: adjusted style to side-bar

--- src/components/macro-components/drawer-menu.tsx
+++ src/components/macro-components/drawer-menu.tsx
@@ -18,13 +18,13 @@ export function DrawerMenu({
     closeSettings
 }: DrawerMenuProps) {
 
-    const [ isOpenDepositModal, setIsOpenDepositModal ] = useState(false);
+    const [isOpenDepositModal, setIsOpenDepositModal] = useState(false);
 
-    function openDepositModal(){
+    function openDepositModal() {
         setIsOpenDepositModal(true);
     }
 
-    function closeDepositModal(){
+    function closeDepositModal() {
         setIsOpenDepositModal(false);
     }
 
@@ -42,18 +42,18 @@ export function DrawerMenu({
     const isClient = role === "ROLE_CLIENT"
 
     return (
-        <div className={`fixed z-50 p-10 bg-red-gradient h-auto w-auto top-0 transition duration-1000 ${isOpen ? 'right-0' : '-right-60'}`}>
-            <div className="flex">
-                <img className="w-60" src={logo} alt="logotipo SwissWallet" />
+        <div className={`fixed z-50 p-5 bg-red-gradient h-full w-[20%] top-0 transition duration-1000 ${isOpen ? 'right-0' : '-right-60'}`}>
+            <div className="flex justify-end pb-3">
+                {/* <img className="w-60" src={logo} alt="logotipo SwissWallet" /> */}
                 <button onClick={closeSettings}>
-                    <X className="size-5 text-white font-medium" />
+                    <X className="size-5 text-white font-medium hover:scale-150 ease-in-out duration-300" />
                 </button>
             </div>
             <div className="bg-white w-full h-[1px]" />
             <div className="w-full flex justify-center mt-5 mb-5">
-                <nav className="flex flex-col items-center justify-center">
+                <nav className="flex flex-col items-center justify-between">
                     <Link to={'/home'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <House className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Home
@@ -61,7 +61,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link to={'/store'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <ShoppingBag className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Store
@@ -69,7 +69,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link to={'/library'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <Book className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Biblioteca
@@ -77,7 +77,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link to={'/canteen'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <Coffee className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Cantina
@@ -85,7 +85,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link to={'/account'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <User className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Conta
@@ -93,7 +93,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link className={`${isClient ? "block" : "hidden"}`} to={'/favorites'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <Heart className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Favoritos
@@ -101,7 +101,7 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link to={'/benefits'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <Gift className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Beneficios
@@ -109,15 +109,15 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <button className={`${isClient ? "hidden" : "block"}`} onClick={openDepositModal}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <Landmark className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
-                                Deposito 
+                                Deposito
                             </span>
                         </div>
-                    </button>   
+                    </button>
                     <Link className={`${isClient ? "block" : "hidden"}`} to={'/orders'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <ShoppingCart className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Pedidos
@@ -125,33 +125,33 @@ export function DrawerMenu({
                         </div>
                     </Link>
                     <Link className={`${isClient ? "hidden" : "block"}`} to={'/list-product'}>
-                        <div className="flex justify-center items-center space-x-2 p-3">
+                        <div className="flex justify-center items-center space-x-2 p-3 hover:scale-110 ease-in-out duration-100">
                             <ShoppingBasket className="text-white size-5 font-bold" />
                             <span className="text-white font-medium">
                                 Produtos
                             </span>
                         </div>
-                    </Link>             
+                    </Link>
                 </nav>
             </div>
 
             <div className="bg-white w-full h-[1px]" />
 
-            <div className="flex mt-5 space-x-5 ">
+            <div className="flex items-center mt-5 flex-col justify-center gap-5">
 
                 <div className="bg-gradient-to-t from-slate-700 to-black flex justify-center items-center rounded-full w-14 h-14 ">
                     <User className="size-10 text-white" />
                 </div>
 
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col justify-centeritems-center">
                     <div className="flex flex-col justify-center items-center">
-                        <h1 className="text-white font-medium text-xl">{user.user.name}</h1>
+                        <h1 className="text-white font-medium text-base">{user.user.name}</h1>
                         <p className="text-zinc-400 font-light">{user.user.username}</p>
 
                     </div>
                     <button onClick={logoutUser}>
-                        <div className="mt-4">
-                            <p className="text-white underline font-medium text-lg">sair</p>
+                        <div>
+                            <p className="text-white underline font-medium text-lg hover:text-[#8b8b8b]">sair</p>
                         </div>
                     </button>
                 </div>
