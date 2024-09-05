43b7af88904bba6b43dc716f975ca9d4a97c3802
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-05 16:33:08 -0300

    feat: navigate to add-product

--- src/components/macro-components/list-product.tsx
+++ src/components/macro-components/list-product.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router';
 import Camisa from '../../assets/images/camisa-branca.svg'
 import Coca from '../../assets/images/coquinha-gelada.svg'
 import { MainButton } from '../micro-components/main-button';
@@ -5,13 +6,20 @@ import RowTable from './colum-row';
 import { HeaderOnPages } from "./header-on-the-pages";
 
 export default function ListProduct() {
+
+    const navigate = useNavigate();
+
+    function click(){
+        navigate('/add-product')
+    }
+
     return (
         <>
             <div className="m-12">
                 <HeaderOnPages 
                     title="Lista de Produtos" 
                     description="Adicione produtos ou remova itens do site"
-                    element={<MainButton width='min' >Novo Produto</MainButton>}
+                    element={<MainButton width='min' onClick={click}>Novo Produto</MainButton>}
                 />
             </div>
             <main className="bg-white shadow-lg m-12 flex flex-col justify-between rounded-3xl">
