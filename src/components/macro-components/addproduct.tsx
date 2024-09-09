eef9aab2712b7b3bdd02a13e8bef6800f6d6ae32
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-09 13:23:00 -0300

    feat: upload image and preview add

--- src/components/macro-components/addproduct.tsx
+++ src/components/macro-components/addproduct.tsx
@@ -1,9 +1,33 @@
-import Cloud      from '../../assets/images/cloud.svg'
+import { UploadCloudIcon, X } from 'lucide-react'
 import { MainButton } from '../micro-components/main-button'
 import { UserInput } from '../micro-components/user-input'
 import { HeaderOnPages } from './header-on-the-pages'
+import { useCallback, useState } from 'react'
+import { DropzoneState, useDropzone } from 'react-dropzone'
+
+interface InputImageProps{
+    dropzone: DropzoneState;
+}
+
+interface HasImageProps {
+    file: File | null;
+}
     
 export function AddNewProduct() {
+
+    const [ file, setFile ] = useState<File | null>(null);
+
+    const onDrop = useCallback((files: File[]) => {
+        setFile(files[0]);
+    }, []);
+
+    const dropzone = useDropzone({
+        onDrop,
+        accept: {
+            "image/jpeg": [".jpg", ".jpeg"],
+        }
+    });
+
     return (
         <div className='m-12'>
             <HeaderOnPages 
@@ -13,10 +37,10 @@ export function AddNewProduct() {
             />
             
             <main className='flex items-center justify-around mt-20'>
-                <button className=' flex flex-col gap-20 hover:bg-zinc-100 bg-white shadow-xl p-16 w-2/4 h-[440px] items-center text-center rounded-xl'>
-                    <p className='text-2xl'>Adicione a foto do produto</p>
-                    <img src={Cloud}  className='w-20'/>
-                </button>
+                
+                {!file ? (<InputImage dropzone={dropzone} />) : (<HasImage file={file}/>)}
+                
+
                 <div className='flex flex-col gap-5 w-2/4 justify-between p-12'>
                     <UserInput>Titulo</UserInput>
                     <UserInput type='number'>Valor</UserInput>
@@ -27,6 +51,36 @@ export function AddNewProduct() {
             <section className='flex justify-center mt-10'>
                 <MainButton>Salvar Produto</MainButton>
             </section>
+        </div>
+    )
+};
+
+const InputImage = ({dropzone}: InputImageProps) => {
+    return(
+        <div 
+        {...dropzone.getRootProps()}
+        className='text-zinc-500 hover:cursor-pointer flex flex-col gap-20 hover:bg-zinc-100 bg-white shadow-xl p-16 w-2/4 h-[440px] items-center text-center rounded-xl'>
+            <label className='text-2xl'>Adicione a foto do produto</label>
+            <UploadCloudIcon className='size-20'/>
+            <input {...dropzone.getInputProps()} className="hidden"/>
+        </div>
+    )
+};
+
+const HasImage = ({file}: HasImageProps) => {
+
+    const previewUrl = file ? URL.createObjectURL(file) : '';
+
+    return(
+        <div className='text-zinc-500  flex flex-col hover:bg-zinc-100 bg-white shadow-xl p-5 gap-1 w-2/4 h-[440px] rounded-xl'>
+            <button className='w-full h-auto flex justify-end'><X className='size-5' /></button>
+            {previewUrl && (
+                <img
+                    src={previewUrl}
+                    alt="Prévia da Imagem"
+                    className="max-w-full max-h-full object-cover rounded flex-1"
+                />
+            )}
         </div>
     )
 }
