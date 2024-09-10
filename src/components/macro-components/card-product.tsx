3da492c612c54be19fce7cc4053e5bba44626bdc
unknown <pedrohenriquefdasilva14@gmail.com>
2024-09-09 21:24:55 -0300

    feat: implement favoriteProduct method in card-product

--- src/components/macro-components/card-product.tsx
+++ src/components/macro-components/card-product.tsx
@@ -29,6 +29,13 @@ export function CardProduct({
 
     console.log(id)
 
+    async function favoriteProduct(){
+        await api.post(`/v3/favorites?idProduct=${id}`)
+        .then(() => {
+            closeCardProduct();
+        })
+    };
+
     async function deleteProduct(){
         await api.delete(`/v3/products?id=${id}`)
         .then(() => {
@@ -56,7 +63,7 @@ export function CardProduct({
                         </div>
                         {isCLient ? (
                             <div className={`flex justify-center`}>
-                                <MainButton width="min">Favoritar</MainButton>
+                                <MainButton width="min" onClick={favoriteProduct}>Favoritar</MainButton>
                             </div>
                         ) : (
                             <div className="space-y-2">
