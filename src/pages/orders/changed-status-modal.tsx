f6d638bc260f06ef07172abf18354efc47f8a6a7
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-09-10 21:51:02 -0300

    style: update style

--- src/pages/orders/changed-status-modal.tsx
+++ src/pages/orders/changed-status-modal.tsx
@@ -21,23 +21,23 @@ export function ChangedStatusModal({
                 <div className="flex flex-col items-center justify-center gap-8">
 
                     <button className="w-full">
-                        <div className="px-5 py-2 hover:bg-slate-100">
+                        <div className="items-start flex px-5 py-2 hover:bg-slate-100">
                             <ProgressStatus/>
                         </div>
                     </button>
                     <button className="w-full">
-                        <div className="hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
-                            <UnavailableStatus/>
+                        <div className="items-start flex hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
+                            <WithDrawStatus/>
                         </div>
                     </button>
                     <button className="w-full">
-                        <div className="hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
+                        <div className="items-start flex hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
                             <CompletedStatus/>
                         </div>
                     </button>
                     <button className="w-full">
-                        <div className="hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
-                            <WithDrawStatus/>
+                        <div className="items-start flex hover:border-slate-400 border-dashed border-slate-500 px-5 py-2 hover:bg-slate-200">
+                            <UnavailableStatus/>
                         </div>
                     </button>
                 </div>
