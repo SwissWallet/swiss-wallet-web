ca473b6ac9eaa3f5e64121c2357516b01c93b238
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-03 19:09:35 -0300

    feat: create CardPayment component

--- src/pages/payment-admin/index.tsx
+++ src/pages/payment-admin/index.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "../../components/macro-components/footer";
 import { Navbar } from "../../components/macro-components/navbar";
+import { CardPayment } from "./payment-card";
 
 export function PaymentAdmin(){
     return(
         <div className="bg-default-gray">
             <Navbar />
-            <main className="flex flex-col ml-20 mr-20 mb-20"></main>
+            <main className="flex flex-col ml-20 mr-20 mb-20">
+                <CardPayment />
+            </main>
             <Footer />
         </div>
     );
---
+++ src/pages/payment-admin/payment-card.tsx
@@ -0,0 +1,7 @@
+export function CardPayment(){
+    return(
+        <div className="w-1/3 h-auto rounded-lg bg-gray-200 hover:bg-gray-300 hover:cursor-pointer">
+            <h1>Payment Card</h1>
+        </div>
+    )
+};
