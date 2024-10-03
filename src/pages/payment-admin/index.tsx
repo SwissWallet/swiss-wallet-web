cfd22041e14fcd5dda729f1c9f1faa8e1e4f1daa
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-03 20:40:21 -0300

    feat: consuming api

--- src/pages/payment-admin/index.tsx
+++ src/pages/payment-admin/index.tsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
 import { Footer } from "../../components/macro-components/footer";
 import { HeaderOnPages } from "../../components/macro-components/header-on-the-pages";
 import { Navbar } from "../../components/macro-components/navbar";
 import { CardPayment } from "./payment-card";
+import { api } from "../../lib/axios";
+
+interface product{
+    id: string;
+    date: string;
+    name: string;
+    value: number;
+    status?: string
+    productName: string;
+}
 
 export function PaymentAdmin(){
+
+    const [ listProducts, setListProducts ] = useState<product[]>([]);
+
+    useEffect(() => {
+        async function getCardProducts(){
+            await api.get(`/v3/order/carts`)
+            .then((json) => {
+                const data = json.data;
+                if(data != ""){
+                    setListProducts(data.map((item: product) => ({
+                        id: item.id,
+                        date: item.dateTime,
+                        name: item.user.name,
+                        status: item.user.role,
+                        productName: item.product[0]?.name || "N/A", // se houver apenas um produto
+                        value: item.product[0]?.value || 0,
+
+                    })))
+                };
+                    
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        };
+        
+        getCardProducts();
+    },[]);
+
+    console.log(listProducts)
+
     return(
         <div className="bg-default-gray">
             <Navbar />
