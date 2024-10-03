75eeb7f34e549cb6db2c90766c3e4442b7dbe664
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-03 19:44:16 -0300

    style: added style on payment card

--- src/pages/payment-admin/index.tsx
+++ src/pages/payment-admin/index.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "../../components/macro-components/footer";
+import { HeaderOnPages } from "../../components/macro-components/header-on-the-pages";
 import { Navbar } from "../../components/macro-components/navbar";
 import { CardPayment } from "./payment-card";
 
@@ -6,7 +7,12 @@ export function PaymentAdmin(){
     return(
         <div className="bg-default-gray">
             <Navbar />
-            <main className="flex flex-col ml-20 mr-20 mb-20">
+            <main className="flex flex-col ml-20 mr-20 mb-20 mt-20 gap-20">
+                <HeaderOnPages
+                    title="Lista de Pagamentos"
+                    description="Lista de pedidos e pagamentos dos usuários"
+                    notFilterAndOrder={true}
+                />
                 <CardPayment />
             </main>
             <Footer />
--- src/pages/payment-admin/payment-card.tsx
+++ src/pages/payment-admin/payment-card.tsx
@@ -1,7 +1,27 @@
+
+
 export function CardPayment(){
     return(
-        <div className="w-1/3 h-auto rounded-lg bg-gray-200 hover:bg-gray-300 hover:cursor-pointer">
-            <h1>Payment Card</h1>
+        <div className="w-1/3 h-auto rounded-lg bg-gray-200 hover:cursor-pointer py-5">
+            <div className="flex justify-between text-lg font-medium px-5">
+                <h1>Nome usuário</h1>
+                <h4>11/12/2022</h4>
+            </div>
+            <div className="mt-2 mb-2 px-10 ">
+                <ol>
+                    <li>produto</li>
+                    <li>produto</li>
+                    <li>produto</li>
+                </ol>
+            </div>
+            <div className="flex justify-between text-lg font-medium px-5">
+                <h3>Valor final: 270 pts</h3>
+                <button
+                    className="bg-red-600 hover:bg-red-700 text-white font-medium text-sm px-2 rounded-md"
+                >
+                    efetuar pagamento
+                </button>
+            </div>
         </div>
     )
 };
