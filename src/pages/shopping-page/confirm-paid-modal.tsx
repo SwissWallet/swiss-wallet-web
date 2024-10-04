be5bf298c047049429cad74e17680d5775e81e36
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-04 19:45:20 -0300

    feat: created confirm paid modal

---
+++ src/pages/shopping-page/confirm-paid-modal.tsx
@@ -0,0 +1,9 @@
+export function ConfirmPaidModal(){
+    return(
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div className="bg-white rounded-lg w-[600px] h-auto p-5 flex gap-8 flex-col">
+                <h1>Hello World</h1>
+            </div>
+        </div>
+    )
+};
