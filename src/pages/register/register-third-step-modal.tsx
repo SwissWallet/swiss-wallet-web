a8cb59196d6f0fa3896f6c9586cd967956964d06
unknown <pedrohenriquefdasilva14@gmail.com>
2024-10-03 22:09:07 -0300

    feat: responsive register step three

--- src/pages/register/register-third-step-modal.tsx
+++ src/pages/register/register-third-step-modal.tsx
@@ -74,7 +74,7 @@ export function RegisterThirdStep({
     }
 
     return (
-        <div className="bg-white rounded-lg w-[600px] h-auto p-8 flex gap-8 flex-col">
+        <div className="bg-white rounded-lg lg:w-[600px] w-3/4 h-auto p-8 flex gap-8 flex-col">
 
             <BackButton onClick={backToTheSecondaryStep} />
 
