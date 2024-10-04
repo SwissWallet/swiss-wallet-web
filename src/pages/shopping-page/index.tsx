c03a3b760df814f6d6ee8ed30e4bdcb816ddac31
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-03 22:04:52 -0300

    feat: finally consuming api

--- src/pages/shopping-page/index.tsx
+++ src/pages/shopping-page/index.tsx
@@ -1,15 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "../../components/macro-components/footer";
 import { HeaderOnPages } from "../../components/macro-components/header-on-the-pages";
 import { Navbar } from "../../components/macro-components/navbar";
 import { api } from "../../lib/axios";
+import { ShoppingCard } from "./shooping-card";
+
+interface product{
+    id: string;
+    date: string;
+    name: string;
+    value: number;
+    status?: string
+    productName: string[];
+}
 
 export function ShoppingPage(){
+
+    const [ listProduct, setListProduct ] = useState<product[]>([]);
 
     useEffect(() => {
         async function getShoppingCart(){
             api.get(`/v3/order/carts/current`)
-            .then((json) => console.log(json))
+            .then((json) =>{
+                const data = json.data;
+                if(data != ""){
+                    setListProduct(data.map((item: product) => ({
+                        id: item.id,
+                        date: item.dateTime,
+                        name: item.user.name,
+                        status: item.status,
+                        productName: item.product.map((productItem) => productItem.name), // se houver apenas um produto
+                        value: item.value || 0,
+
+                    })))
+                };
+            })
             .catch((err) => console.log(err))
         };
 
@@ -26,6 +51,19 @@ export function ShoppingPage(){
                     notFilterAndOrder={true}
                 />
 
+                <>
+                    {listProduct.length > 0 && (
+                        listProduct.map((item) => (
+                            <div className="flex flex-col gap-6" key={item.id}>
+                                <ShoppingCard
+                                    dateTime={item.date}
+                                    productName={item.productName}
+                                    status={item.status || ""}
+                                />
+                            </div>
+                        ))
+                    )}
+                </>
                 
             </main>
             <Footer />
--- src/pages/shopping-page/shooping-card.tsx
+++ src/pages/shopping-page/shooping-card.tsx
@@ -1,17 +1,26 @@
-export function ShoppingCard(){
+interface ShoppingCardProps{
+    id?: string;
+    dateTime: string;
+    productName: string[];
+    status: string;
+    value?: number;
+}
+
+export function ShoppingCard({
+    dateTime,
+    productName,
+    status,
+}: ShoppingCardProps){
     return(
                 <div className="w-full h-auto flex flex-col px-8 justify-between rounded-lg font-medium text-2xl py-10 bg-gray-200 hover:cursor-pointer">
-                    <h4 className="text-end">11/12/2022</h4>
+                    <h4 className="text-end">{dateTime}</h4>
 
                     <div className="mt-8">
-                        <ol>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                            <li><span className="font-normal text-xl">produto 1</span></li>
-                        </ol>
+                    <ol>
+                        {productName.map((product, index) => (
+                            <li key={index}>{product}</li>
+                        ))}
+                    </ol>
                     </div>
 
                     <div className="w-full h-[1px] mt-8 bg-gray-800"/>
@@ -19,10 +28,10 @@ export function ShoppingCard(){
                     <div className="flex mt-8 justify-between">
                         <div className="text-center flex flex-col gap-4">
                             <h1 className="text-zinc-700">pagamento:</h1>
-                            <h1 className="text-zinc-700">PENDENTE</h1>
+                            <h1 className="text-zinc-700">{status}</h1>
                         </div>
                         <button
-                            className="bg-red-600 hover:bg-red-700 px-10 py-2 rounded-md text-white-90"
+                            className="bg-red-600 hover:bg-red-700 px-10 py-2 rounded-md text-white-90 font-medium"
                         >
                             REALIZAR PAGAMENTO
                         </button>
