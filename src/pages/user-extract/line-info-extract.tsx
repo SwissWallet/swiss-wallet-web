ff2e54522c8f032424a1384feb5842b3eeb2761d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-09 20:34:15 -0300

    fix: changed lg-flex-col to lg:flex-col at extract page

--- src/pages/user-extract/line-info-extract.tsx
+++ src/pages/user-extract/line-info-extract.tsx
@@ -7,15 +7,30 @@ interface InfoExtractProps {
 export function InfoExtract({ icon, value, date }: InfoExtractProps) {
     return (
         <div className="flex flex-col p-3 gap-2">
+
+
+
+
             <div className="flex justify-between items-center flex-col md:flex-row ">
                 <h1 className="text-2xl font-medium italic hidden lg:flex">{value} pontos</h1>
-                <div className="flex lg-flex-col gap-5">
+                <div className="flex lg:flex-col gap-5">
                     {icon}
                     <h1 className="text-2xl font-medium italic flex lg:hidden">{value}</h1>
                 </div>
                 <p className="font-bold italic text-black text-opacity-75 text-center">{date}</p>
             </div>
+
+
+
+
+
+            
             <div className="w-full h-[1px] bg-slate-400 " />
+
+
+
+
+
         </div>
     )
 }
