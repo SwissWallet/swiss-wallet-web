8a3e729c7ab3ef074cb8bd129aa2d66403024100
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-10 20:22:04 -0300

    style: update style card bank

--- src/components/macro-components/card-bank-details.tsx
+++ src/components/macro-components/card-bank-details.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import background from "../../assets/images/background-card-bank.png";
 import visa from "../../assets/images/visa-logo.svg";
 
 interface CardBankDetailsProps{
@@ -10,17 +11,27 @@ export function CardBankDetails({
 }: CardBankDetailsProps){
     return(
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-            <div className="bg-black text-white rounded-lg w-[333px] rounded-t-lg flex flex-col gap-5">
-                <div className="flex justify-between text-2xl items-center p-5">
-                    <h1>Saldo</h1>
+            <div className="relative bg-black text-white rounded-lg w-[333px] h-[200px] rounded-t-lg flex flex-col gap-5">
+                <div className="flex justify-between items-center p-5">
+                    <h1 className="text-[11px] [#a7a7a7}">Saldo</h1>
                     <button onClick={() => setOpenDetailsCard(false)}>
-                        <X className="size-8"/>
+                        <X className="size-4"/>
                     </button>
                 </div>
-                <h1>$ <span>123,00</span></h1>
-                <div className="bg-[#c50000] rounded-b-lg flex justify-between px-5">
-                    <h1>**** 1234</h1>
-                    <img src={visa} />
+                <div className="relative h-full">
+                    <img 
+                        src={background} 
+                        className="absolute -top-11 right-0 -mb-20 size-36 object-cover z-0 opacity-50"
+                    />
+                    <div className="absolute top-0 left-5 z-10">
+                        <h1 className="text-3xl">$ <span className="text-5xl">123</span><span className="text-2xl">,00</span></h1>
+                    </div>
+                </div>
+                <div className="absolute bottom-0 w-full h-[60px] bg-[#c50000] rounded-b-lg flex justify-between px-5">
+                    <div className="flex justify-between w-full items-center">
+                        <h1 className="text-2xl">**** 1234</h1>
+                        <img src={visa} />
+                    </div>
                 </div>
             </div>
         </div>
