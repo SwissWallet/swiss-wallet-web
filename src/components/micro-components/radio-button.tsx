72aaae0d112858da8c4c38b7c69b414cbe5fce8a
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-09 21:41:40 -0300

    style: update styles on RadioButton

--- src/components/micro-components/radio-button.tsx
+++ src/components/micro-components/radio-button.tsx
@@ -3,28 +3,28 @@ import { useState } from 'react';
 export function RadioButton(){
   const [selectedOption, setSelectedOption] = useState('');
 
-  const handleOptionChange = (event: React.FormEvent) => {
-    setSelectedOption(event.target.value);
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
   };
 
   return (
     <div className="flex justify-center items-center gap-10">
-      {['debit', 'credit', 'pix'].map((option) => (
-        <div key={option}>
+      {['debito', 'credito', 'pix'].map((option) => (
+        <div  className={`
+            flex items-center rounded-md py-3 px-5 transition-all duration-300 cursor-pointer
+            ${selectedOption === option ? 'scale-110 bg-red-gradient' : 'border-4 border-gray-500'
+          }`} 
+          onClick={() => handleOptionChange(option)}
+          key={option}>
           <label className="relative inline-flex items-center cursor-pointer">
             <input
               type="radio"
               value={option}
               checked={selectedOption === option}
-              onChange={handleOptionChange}
+              onChange={() => handleOptionChange(option)}
               className="sr-only peer"
             />
-            <div className={`w-6 h-6 border-2 border-gray-300 rounded-full peer-checked:border-blue-500 flex items-center justify-center`}>
-              {selectedOption === option && (
-                <div className="w-3 h-3 bg-blue-500 rounded-full"></div>
-              )}
-            </div>
-            <span className="ml-2 capitalize">{option}</span>
+            <span className={`ml-2 capitalize text-xl ${selectedOption === option ? 'text-white font-bold' : "text-gray-500"}`}>{option}</span>
           </label>
         </div>
       ))}
