70115dd7f0b69a82e2ef81896d755b04c3147dca
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-10 21:04:48 -0300

    feat: checking if is client or admin at benefit page

--- src/pages/benefits/index.tsx
+++ src/pages/benefits/index.tsx
@@ -1,19 +1,34 @@
+import { useSelector } from "react-redux";
 import { Footer } from "../../components/macro-components/footer";
 import { HeaderOnPages } from "../../components/macro-components/header-on-the-pages";
 import { Navbar } from "../../components/macro-components/navbar";
+import { MainButton } from "../../components/micro-components/main-button";
+import { RootState } from "../../store";
 import { BenefitsCard } from "./benefits-card";
 
 
 export function Benefits() {
+
+    const user = useSelector((state: RootState) => state.authUser.value);
+
+    const role = user.user.role;
+    const isClient = role === "ROLE_CLIENT";
+
     return (
         <div className="bg-default-gray">
             <Navbar />
             <main className="ml-20 mr-20 gap-20 flex flex-col mt-20 mb-20">
 
-                <HeaderOnPages
-                    title="Beneficíos"
-                    description="Confira a lista de beneficios de assinantes da AAPM"
-                />
+                <div className="flex justify-between">
+                    <HeaderOnPages
+                        title="Beneficíos"
+                        description="Confira a lista de beneficios de assinantes da AAPM"
+                        notFilterAndOrder={true}
+                    />
+                    <div className={`flex gap-10 items-center ${isClient ? "hidden" : "block"}`}>
+                        <MainButton width="min">Novo Benefício</MainButton>
+                    </div>
+                </div>
 
                 <BenefitsCard
                     benefitsName="Beneficio VT"
