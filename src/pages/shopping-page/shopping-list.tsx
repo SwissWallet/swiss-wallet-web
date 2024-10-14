7413543ff0e742586216f52cc199e2e9c2af82d1
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-14 18:59:36 -0300

    feat: added func filter and created ShoppingList component

---
+++ src/pages/shopping-page/shopping-list.tsx
@@ -0,0 +1,50 @@
+import { product } from "."
+import { ShoppingCard } from "./shopping-card";
+
+interface ShoppingListProps{
+    getShoppingCart: () => void;
+    listProduct: product[];
+    filter: string;
+}
+
+export function ShoppingList({
+    listProduct,
+    filter,
+    getShoppingCart,
+}: ShoppingListProps){
+
+    return(
+        listProduct.length > 0 && (
+            filter ? (
+                listProduct
+                .filter((product) => product.status === filter)
+                .map((item) => (
+                    <div className="flex flex-col gap-6" key={item.id}>
+                        <ShoppingCard
+                            id={item.id}
+                            dateTime={item.date}
+                            productName={item.productName}
+                            status={item.status === "PENDING" ? ("PENDENTE") : ("PAGO")}
+                            value={item.value}
+                            getShoppingCart={getShoppingCart}
+                        />
+                    </div>
+                ))
+            ) : (
+                listProduct
+                .map((item) => (
+                    <div className="flex flex-col gap-6" key={item.id}>
+                        <ShoppingCard
+                            id={item.id}
+                            dateTime={item.date}
+                            productName={item.productName}
+                            status={item.status === "PENDING" ? ("PENDENTE") : ("PAGO")}
+                            value={item.value}
+                            getShoppingCart={getShoppingCart}
+                        />
+                    </div>
+                ))
+            )
+        )
+    )
+}
