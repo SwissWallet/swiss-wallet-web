57d201b9b1d73353d33e011dd1d188f41f9f3288
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-16 20:07:41 -0300

    fix: useEffect add dependecy

--- src/pages/benefits/index.tsx
+++ src/pages/benefits/index.tsx
@@ -57,7 +57,7 @@ export function Benefits() {
         }else{
             getBenefitExistent();
         }
-    }, [])
+    }, [isClient])
 
     return (
         <div className="bg-default-gray">
