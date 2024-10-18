589fd08c1dca912296ea06e85696da040e4fd740
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-17 21:58:04 -0300

    feat: changed status

--- src/pages/benefits/benefit-card-requests.tsx
+++ src/pages/benefits/benefit-card-requests.tsx
@@ -1,5 +1,6 @@
-import { MainButton } from "../../components/micro-components/main-button";
+
 import { api } from "../../lib/axios";
+import { UserSelect } from "../../components/micro-components/category-input";
 
 interface BenefitsCardProps {
     id: string;
@@ -20,10 +21,10 @@ export function BenefitsCardRequests({
     idRequest,
 }: BenefitsCardProps) {
 
-    async function updateStatus(idRequest: string){
+    async function updateStatus(cstatus: string, idRequest: string){
         await api.put(`/v3/benefit/requests`, {
             idBenefit: idRequest,
-            status: "IN_PROGRESS"
+            status: cstatus
         })
         .then(() => console.log("passou"))
         .catch((err) => console.log(err))
@@ -37,24 +38,25 @@ export function BenefitsCardRequests({
                     
                     <div className="flex justify-between px-5">
                         <h1 className="text-2xl font-semibold">{title}</h1>
-                        
-                            <h1 className="text-xl font-semibold">{dateTime}</h1>
-                        
+                        <h1 className="text-xl font-semibold">{dateTime}</h1>
                     </div>
                     <p className="w-full overflow-hidden italic font-medium px-10">
                         {description}
                     </p>
                 </div>
-                <div className="flex justify-end">
-                    
+                <div className="flex items-center justify-end">
+                        <h1 className="text-2xl text-red-600 font-bold px-10">{status}</h1>
                         <div className="flex gap-10 items-center">
-                            
-                            
-                                <MainButton onClick={() => updateStatus(idRequest)} width="min">
-                                    Alterar status
-                                </MainButton>
-                            
-                            
+                            <UserSelect
+                                onChange={(e) => updateStatus(e.target.value, idRequest)}
+                                firstMessage="altere o status"
+                                options={[
+                                    {key: "SEND", value: "ENVIADO"},
+                                    {key: "IN_PROGRESS", value: "EM PROGRESSO"},
+                                    {key: "APPROVED", value: "APROVADO"},
+                                    {key: "NOT_APPROVED", value: "NÃO APROVADO"},
+                                ]}
+                            ></UserSelect>
                         </div>
                     
                 </div>
