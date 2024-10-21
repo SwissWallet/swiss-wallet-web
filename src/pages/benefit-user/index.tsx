1138fc85b9fb3db8eb785d1260f37ad81a85c292
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-21 19:45:30 -0300

    feat: consuming api

--- src/pages/benefit-user/index.tsx
+++ src/pages/benefit-user/index.tsx
@@ -3,10 +3,49 @@ import { Footer } from "../../components/macro-components/footer";
 import { HeaderOnPages } from "../../components/macro-components/header-on-the-pages";
 import { Navbar } from "../../components/macro-components/navbar";
 import { UserSelect } from "../../components/micro-components/category-input";
+import { api } from "../../lib/axios";
+
+interface benefit{
+    id: string;
+    title: string;
+    description: string;
+    status?: string;
+    dateTime?: string;
+    benefitActive?: {
+        id: string;
+        title: string;
+        description: string;
+    }
+};
 
 export function BenefitUser(){
 
     const [ filter, setFilter ] = useState<"ACTIVE" | "REQUEST" | "">("");
+    const [ benefits, setBenefits ] = useState<benefit>();
+    
+    async function getBenefitActiveClient(){
+        api.get(`/v3/benefit/requests/current`)
+        .then((json) => {
+            const data = json.data;
+            console.log(data);
+            setBenefits(data.activeResponseDtos.map((benefit: benefit) => ({
+                id: benefit.id,
+                title: benefit.title,
+                description: benefit.description
+            })))
+            setBenefits(data.reqResponseDtos.map((benefit: benefit) => ({
+                id: benefit.id,
+                status: benefit.status,
+                dateTime: benefit.dateTime,
+                benefitActive: {
+                    id: benefit.benefitActive.id,
+                    title: benefit.benefitActive.title,
+                    description: benefit.benefitActive.description
+                }
+            })))
+        })
+    };
+
 
 
     return(
@@ -32,6 +71,8 @@ export function BenefitUser(){
                         />
                     </div>
                 </div>
+
+                
 
             </main>
             <Footer />
