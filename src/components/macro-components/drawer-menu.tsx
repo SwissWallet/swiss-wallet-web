fe0016defb770e56503d11a0c49dcdc9e8756a0b
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-11-05 19:20:59 -0300

    fix: width drawer menu responsivity

--- src/components/macro-components/drawer-menu.tsx
+++ src/components/macro-components/drawer-menu.tsx
@@ -54,9 +54,8 @@ export function DrawerMenu({
     const isClient = role === "ROLE_CLIENT"
 
     return (
-        <div className={`fixed z-50 p-5 bg-red-gradient h-full w-[20%] top-0 transition duration-1000 ${isOpen ? 'right-0' : '-right-60'}`}>
+        <div className={`fixed z-50 p-5 bg-red-gradient h-full lg:w-[20%] top-0 transition duration-1000 ${isOpen ? 'right-0' : '-right-60'}`}>
             <div className="flex justify-end pb-3">
-                {/* <img className="w-60" src={logo} alt="logotipo SwissWallet" /> */}
                 <button onClick={closeSettings}>
                     <X className="size-5 text-white font-medium hover:scale-150 ease-in-out duration-300" />
                 </button>
