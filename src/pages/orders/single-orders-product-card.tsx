e2eb325694f13324b92b3f4fe479276519b08064
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-11-05 20:05:08 -0300

    fix: adding gap beetwen single order product and checkbox

--- src/pages/orders/single-orders-product-card.tsx
+++ src/pages/orders/single-orders-product-card.tsx
@@ -3,8 +3,6 @@
     import { MainButton } from "../../components/micro-components/main-button";
     import { OrderCardProduct } from "./order-card-product";
     import { CheckProduts } from ".";
-    import { useSelector } from "react-redux";
-    import { RootState } from "../../store";
     import { Checkbox } from "../../components/micro-components/checkbox";
 
     interface SingleOrdersProductCardProps {
@@ -26,28 +24,20 @@
         image,
         value,
         orderId,
-        username,
         description,
         handdleSelectProducts,
     }: SingleOrdersProductCardProps) {
 
         const [ openOrderCard, setOpenOrderCard ] = useState(false);
 
-        const user = useSelector((state: RootState) => state.authUser.value);
-
-        const role = user.user.role;
-        const isClient = role === "ROLE_CLIENT";
-
         return (
-            <div className="flex">
-                <div className={`${isClient ? "block" : "hidden"}`}>
+            <div className="flex gap-2">
+
                 <Checkbox 
                     handleSelectProduct={(isSelect) => handdleSelectProducts(isSelect, id, title, value)}
                 />
 
-                </div>
                 <div className="flex items-center flex-col box-border gap-10">
-                    <span className={`font-medium text-lg ${isClient ? "hidden" : "block"}`}>de: {username}</span>
                     <img src={image} className="w-[309px] h-[407px]" alt="camiseta branca com logo do senai" />
                     <article className="bg-black text-white p-4 rounded-lg gap-4 flex flex-col lg:w-[320px] w-[200px] -mt-36 px-10">
                         <div className="flex flex-col gap-1 text-center ">
