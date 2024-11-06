f5c82938d260120cddcabce329a9cbb9d7de3394
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-11-05 21:25:13 -0300

    fix: replacing display grid to display flex index benefit

--- src/pages/benefit-admin/benefit-card-requests.tsx
+++ src/pages/benefit-admin/benefit-card-requests.tsx
@@ -14,7 +14,7 @@ export function BenefitCardRequests({
   description,
 }: BenefitCardRequestsProps) {
   return (
-    <section className="mx-4 sm:mx-6 w-full lg:mx-12 xl:mx-20">
+    <section className="mx-4 sm:mx-6 lg:mx-12 xl:mx-20">
       <div className="flex flex-col bg-white p-5 drop-shadow-custom rounded-md gap-6">
         <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center text-sm sm:text-base font-medium border-b-2 pb-2">
           <h1 className="text-lg sm:text-2xl font-bold">{name}</h1>
--- src/pages/benefit-admin/index.tsx
+++ src/pages/benefit-admin/index.tsx
@@ -92,7 +92,7 @@ export function BenefitAdmin() {
                     benefitsRequests.length < 1 ? (
                         <NoProducts />
                     ) : (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        <div className="flex flex-col gap-6">
                             {benefitsRequests.map((benefit: benefit) => (
                                 <BenefitCardRequests
                                     key={benefit.id}
